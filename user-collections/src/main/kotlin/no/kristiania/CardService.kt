02daeda6f80c0719dab8cb1dc513936d72a943d8
Fredrik Holanger <fholange@gmail.com>
2020-10-06 17:08:54 +0200

    something

--- user-collections/src/main/kotlin/no/kristiania/CardService.kt
+++ user-collections/src/main/kotlin/no/kristiania/CardService.kt
@@ -4,6 +4,7 @@ import no.kristiania.model.Card
 import no.kristiania.model.Collection
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import javax.annotation.PostConstruct
 
 @Service
 class CardService {
@@ -15,6 +16,28 @@ class CardService {
 
     val cardCollection : List<Card>
         get() = collection?.cards ?: listOf()
+    private val lock = Any()
 
-    fun isInitialized() = false
+    @PostConstruct
+    fun init(){
+        synchronized(lock){
+            if(cardCollection.isNotEmpty()){
+                return
+            }
+            fetchData()
+        }
+    }
+
+    fun isInitialized() = cardCollection.isNotEmpty()
+    protected fun fetchData(){
+        //todo
+    }
+
+    private fun verifyCollection(){
+
+    }
+
+
+
+
 }
