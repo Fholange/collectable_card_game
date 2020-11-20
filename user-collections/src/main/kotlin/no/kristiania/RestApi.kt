f526dbe5a95e7d05f875d4a77bc45d0918db6fc3
Fholange <51076892+Fholange@users.noreply.github.com>
2020-11-20 20:23:28 +0100

    sdfd

--- user-collections/src/main/kotlin/no/kristiania/RestApi.kt
+++ user-collections/src/main/kotlin/no/kristiania/RestApi.kt
@@ -2,13 +2,11 @@ package no.kristiania
 
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
+import no.kristiania.db.UserService
 import no.kristiania.dto.UserDto
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @Api(value = "/api/user-collections", description = "Operations on card collections owned by users")
 @RequestMapping(
@@ -16,15 +14,31 @@ import org.springframework.web.bind.annotation.RestController
         produces = [(MediaType.APPLICATION_JSON_VALUE)]
 )
 
-/*@RestController
-class RestApi(private val userService: CardService) {
+@RestController
+class RestApi(private val userService: UserService) {
 
     @ApiOperation("Retrieve card collection information for a specific user")
     @GetMapping(path = ["/{userId}"])
     fun getUserInfo(
             @PathVariable("userId") userId : String
     ) : ResponseEntity<UserDto>{
+        val user = userService.findByIdEager(userId)
+        if(user == null){
+            return ResponseEntity.notFound().build()
+        }
 
-    }*/
+        return ResponseEntity.status(200).body(DtoConverter.transform(user))
+
+    }
+
+    @ApiOperation("Create a new user, with the given id")
+    @PutMapping(path = ["/{userId}"])
+    fun createUser(
+            @PathVariable("userId") userId: String
+    ) : ResponseEntity<Void>{
+        val ok = userService.registerNewUser(userId)
+        return if(!ok) ResponseEntity.status(400).build()
+            else ResponseEntity.status(201).build()
+    }
 
 }
--- user-collections/src/main/kotlin/no/kristiania/dto/CardCopyDto.kt
+++ user-collections/src/main/kotlin/no/kristiania/dto/CardCopyDto.kt
@@ -3,8 +3,10 @@ package no.kristiania.dto
 import io.swagger.annotations.ApiModelProperty
 
 data class CardCopyDto(
+
         @get:ApiModelProperty("Id of the Card")
         var cardId: String? = null,
+
         @get:ApiModelProperty("Number of copies of the card that the user owns")
         var numberOfCopies: Int? = null
 )
